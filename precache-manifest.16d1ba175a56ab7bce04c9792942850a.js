dee1825e80467623227922c62f944e3792e1a259
Alex Nabzdorov <Sanek_109@list.ru>
2020-04-14 15:32:34 +0300

    Updates

---
+++ precache-manifest.16d1ba175a56ab7bce04c9792942850a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e368aefdb90df012c7271344c6eb07",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "136d0163cd58b71abe70",
+    "url": "/todolist/static/css/main.6c616a3b.chunk.css"
+  },
+  {
+    "revision": "9fec86b79d560e4e12a7",
+    "url": "/todolist/static/js/2.22b3eed0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.22b3eed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136d0163cd58b71abe70",
+    "url": "/todolist/static/js/main.8f269e75.chunk.js"
+  },
+  {
+    "revision": "5755bba08db18ac713b7",
+    "url": "/todolist/static/js/runtime-main.66743db1.js"
+  }
+]);
